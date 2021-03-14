ce67b73c6204068ab691c489362968f26409ae44
Ilya01 <illyaerohin@yandex.ru>
2021-03-14 08:07:28 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 public class Main {
-    public static void offer(int bill_balance, int replenishment) {
-        int account_status = 100;
+    public static void offer(int billBalance, int replenishment) {
+        int accountStatus = 100;
         int limit = 1000;
         if (replenishment >= limit) {
-            int bonus = replenishment / account_status;
-            bill_balance += replenishment + bonus;
-            System.out.println(" Пополнение на " + replenishment + " рублей.");
+            int bonus = replenishment / accountStatus;
+            billBalance += replenishment + bonus;
             System.out.println(" Поздравляем! Вы получули " + bonus + " бонусных рублей. ");
-            System.out.println(" Сумма вашего баланса " + bill_balance + " Рублей. ");
         } else {
-            bill_balance += replenishment;
-            System.out.println(" Пополнение на " + replenishment + " рублей ");
+            billBalance += replenishment;
             System.out.println(" Бонусы не будут начислены. ");
-            System.out.println(" Баланс вашего счета: " + bill_balance + " рублей. ");
         }
+        System.out.println(" Пополнение на " + replenishment + " рублей.");
+        System.out.println(" Сумма вашего баланса " + billBalance + " Рублей. ");
     }
-        public static void main(String[] args) {
-            offer(100, 1000);
-            offer(100, 100);
+    public static void main(String[] args) {
+        offer(100, 1000);
+        offer(100, 100);
         }
     }
